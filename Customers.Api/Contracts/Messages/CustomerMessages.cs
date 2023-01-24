7519f5caeda4e1e36747180258514d9778590e7a
dimitriuz <dimitriuz@gmail.com>
2023-01-24 22:23:07 +0300

    Customers API messaging

---
+++ Customers.Api/Contracts/Messages/CustomerMessages.cs
@@ -0,0 +1,34 @@
+﻿namespace Customers.Api.Contracts.Messages
+{
+    public class CustomerCreated
+    {
+        public Guid Id { get; init; } = default!;
+
+        public string GitHubUsername { get; init; } = default!;
+
+        public string FullName { get; init; } = default!;
+
+        public string Email { get; init; } = default!;
+
+        public DateTime DateOfBirth { get; init; }
+    }
+
+    public class CustomerUpdated
+    {
+        public Guid Id { get; init; } = default!;
+
+        public string GitHubUsername { get; init; } = default!;
+
+        public string FullName { get; init; } = default!;
+
+        public string Email { get; init; } = default!;
+
+        public DateTime DateOfBirth { get; init; }
+    }
+
+    public class CustomerDeleted
+    {
+        public Guid Id { get; init; } = default!;
+
+    }
+}
---
+++ Customers.Api/Messaging/SqsMessenger.cs
@@ -0,0 +1,52 @@
+﻿using Amazon.SQS;
+using Amazon.SQS.Model;
+using Microsoft.Extensions.Options;
+using System.Text.Json;
+
+namespace Customers.Api.Messaging
+{
+    public class SqsMessenger : ISqsMessenger
+    {
+        private readonly IAmazonSQS _sqs;
+        private readonly IOptions<QueueSettings> _queueSettings;
+        private string? _queueUrl;
+
+        public SqsMessenger(IAmazonSQS sqs, IOptions<QueueSettings> queueSettings)
+        {
+            _sqs = sqs;
+            _queueSettings = queueSettings;
+        }
+
+        public async Task<SendMessageResponse> SendMessageAsync<T>(T message)
+        {
+            var queueUrl = await GetQueueUrlAsync();
+
+            var sendMessageRequest = new SendMessageRequest
+            {
+                QueueUrl = queueUrl,
+                MessageBody = JsonSerializer.Serialize(message),
+                MessageAttributes = new Dictionary<string, MessageAttributeValue> {
+                    {
+                        "MessageType", new MessageAttributeValue
+                        {
+                            DataType = "String",
+                            StringValue = typeof(T).Name
+                        }
+                    } }
+            };
+
+            var ct = new CancellationTokenSource();
+            return await _sqs.SendMessageAsync(sendMessageRequest, ct.Token);
+        }
+
+        private async Task<string> GetQueueUrlAsync()
+        {
+            if (_queueUrl is null) {
+                var queueUrlResponse = await _sqs.GetQueueUrlAsync(_queueSettings.Value.Name);
+                _queueUrl = queueUrlResponse.QueueUrl;
+            };
+
+            return _queueUrl;
+        }
+    }
+}
