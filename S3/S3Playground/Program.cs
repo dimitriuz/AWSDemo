adaf8d35384f41ff9c00bc9e19324672b9915c1e
dimitriuz <dimitriuz@gmail.com>
2023-02-02 21:42:16 +0300

    S3 upload&download simple playground

---
+++ S3/S3Playground/Program.cs
@@ -0,0 +1,52 @@
+﻿using Amazon.S3;
+using Amazon.S3.Model;
+using System.Text;
+
+internal class Program
+{
+    private static async Task Main(string[] args)
+    {
+        //await UploadFile();
+        await DownloadFile();
+    }
+
+    private static async Task UploadFile()
+    {
+        var s3client = new AmazonS3Client();
+
+        await using var inputStream = new FileStream("./movies.csv", FileMode.Open, FileAccess.Read);
+
+        var putObjectRequest = new PutObjectRequest
+        {
+            BucketName = "ldsoft",
+            Key = "files/movies.csv",
+            ContentType = "text/csv",
+            InputStream = inputStream
+        };
+
+        await s3client.PutObjectAsync(putObjectRequest);
+    }
+
+    private static async Task DownloadFile()
+    {
+        var s3client = new AmazonS3Client();
+
+
+        var getObjectRequest = new GetObjectRequest
+        {
+            BucketName = "ldsoft",
+            Key = "files/movies.csv"
+        };
+
+        var response = await s3client.GetObjectAsync(getObjectRequest);
+
+        var memoryStream = new MemoryStream();
+
+        response.ResponseStream.CopyTo(memoryStream);
+
+        var text = Encoding.Default.GetString(memoryStream.ToArray());
+
+        Console.WriteLine(text);
+    }
+
+}
