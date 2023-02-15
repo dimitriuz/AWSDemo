215d4d6418f473f43172bf5e49843ecfd58cfbf5
dimitriuz <dimitriuz@gmail.com>
2023-02-15 21:12:19 +0300

    Secrets Manager playground app

---
+++ SecretsManager/SecretsManager.App/Program.cs
@@ -0,0 +1,32 @@
+﻿using Amazon.SecretsManager;
+using Amazon.SecretsManager.Model;
+
+namespace SecretsManager.App
+{
+    internal class Program
+    {
+        static async Task Main(string[] args)
+        {
+            var smClient = new AmazonSecretsManagerClient();
+           
+            var secretValueRequest = new GetSecretValueRequest
+            {
+                SecretId = "ApiKey",
+                VersionStage = "AWSCURRENT" //"AWSPREVIOUS"
+            };
+
+            var secretValueResponse = await smClient.GetSecretValueAsync(secretValueRequest);
+
+            Console.WriteLine(secretValueResponse.SecretString);
+
+            var describeSecretRequest = new DescribeSecretRequest
+            {
+                SecretId = "ApiKey"
+            };
+
+            var describeSecretResponse = await smClient.DescribeSecretAsync(describeSecretRequest);
+
+            Console.WriteLine(describeSecretResponse.CreatedDate);
+        }
+    }
+}
