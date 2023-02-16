29752f1ba7eff74e41df631706d44b8b5ea600c5
dimitriuz <dimitriuz@gmail.com>
2023-02-16 23:21:44 +0300

    simple lambda function

---
+++ Lambda/SimpleLambda/src/SimpleLambda/Function.cs
@@ -0,0 +1,22 @@
+using Amazon.Lambda.Core;
+// Assembly attribute to enable the Lambda function's JSON input to be converted into a .NET class.
+[assembly: LambdaSerializer(typeof(Amazon.Lambda.Serialization.SystemTextJson.DefaultLambdaJsonSerializer))]
+namespace SimpleLambda
+{
+    //for deploy use - dotnet Lambda deploy-function SimpleLambda
+    //for invoke use - dotnet Lambda invoke-function SimpleLambda
+
+    public class Function
+    {
+        public string FunctionHandler(Hello request, ILambdaContext context)
+        {
+            context.Logger.LogInformation($"Hello from {request.World}");
+            return request.World;
+        }
+    }
+
+    public class Hello
+    {
+        public string World { get; set; } = default!;
+    }
+}
